30aec24302c494519efc1f6982bd9b4923eff7f9
Kristiina <krsama@taltech.ee>
2021-03-04 19:55:15 +0200

    added dto

---
+++ src/main/java/ee/taltech/calculator/dto/CalculationResult.java
@@ -0,0 +1,15 @@
+package ee.taltech.calculator.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+public class CalculationResult {
+    private Integer maxEven;
+    private Integer minOdds;
+    private List<Integer> even;
+    private List<Integer> squared;
+    private Float averageOfPositives;
+}
