85a892238efe5132ca06375f7b69652bffd3abf4
Kristiina <krsama@taltech.ee>
2021-03-04 19:49:11 +0200

    completed service

--- src/main/java/ee/taltech/calculator/service/CalculatorService.java
+++ src/main/java/ee/taltech/calculator/service/CalculatorService.java
@@ -3,6 +3,8 @@ package ee.taltech.calculator.service;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,18 +18,45 @@ public class CalculatorService {
         }
         return evens;
     }
-    public Float averageOfPositives(List<Float> numbers){
-        List<Float> positives = new ArrayList<>();
-        for (Float number : numbers) {
+    public Float averageOfPositives(List<Integer> numbers){
+        List<Integer> positives = new ArrayList<>();
+        for (Integer number : numbers) {
             if(number > 0){
                 positives.add(number);
             }
         }
         float sum = 0;
-        for (Float positive : positives) {
+        for (Integer positive : positives) {
             sum = positive + sum;
         }
         sum = sum / positives.size();
         return sum;
+    }
+    public Integer maxEven(List<Integer> numbers){
+        List<Integer> evens = new ArrayList<>();
+        for (Integer integer : numbers) {
+            if(integer %2 == 0) {
+                evens.add(integer);
+            }
+        }
+        int max = Collections.max(evens);
+        return max;
+    }
+    public Integer minOdd(List<Integer> numbers){
+        List<Integer> odds = new ArrayList<>();
+        for (Integer integer : numbers) {
+            if(integer %2 == 1) {
+                odds.add(integer);
+            }
+        }
+        int min = Collections.min(odds);
+        return min;
+    }
+    public List<Integer> squared(List<Integer> integers){
+        List<Integer> squared = new ArrayList<>();
+        for (Integer integer : integers) {
+            squared.add(integer*integer);
+        }
+        return squared;
     }
 }
