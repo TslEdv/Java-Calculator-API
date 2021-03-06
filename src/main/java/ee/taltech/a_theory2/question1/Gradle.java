cac220a45c2cfadfb49c7ccc6a2f549a4e0eb590
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-03-06 19:26:48 +0200

    answered theory 1 about gradle

--- src/main/java/ee/taltech/a_theory2/question1/Gradle.java
+++ src/main/java/ee/taltech/a_theory2/question1/Gradle.java
@@ -8,17 +8,17 @@ public class Gradle {
     //todo A
     // In your words (do not copy wiki definitions)
     // What is gradle?
-    // Answer:
+    // Answer: it is an utility to build (convert source code to machine code) projects in languages such as Java.
 
     //todo B
     // What is gradle wrapper? Why are we using it instead of gradle?
-    // Answer:
+    // Answer: Gradle has many different incompatible versions. The wrapper allows a project to declare a specific version, and install it if needed.
 
     //todo C
     // Name 3 different benefits or reasons for using gradle
-    // 1
-    // 2
-    // 3
+    // 1 Gradle keeps track of the dependencies used by the project
+    // 2 Gradle employs tricks such as incrementality and build cache to speed up the build process. Can be several times faster than maven
+    // 3 Build scripts are easily readable, which speeds up debugging
 
     //todo D
     // Name at least 1 alternative to gradle
