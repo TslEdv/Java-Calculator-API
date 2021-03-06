708dda0fc7340bcb65efc82e1c5af0735b7bde4f
Edvin Ess <edvess@taltech.ee>
2021-03-06 19:17:42 +0200

    feedback2

--- src/main/java/ee/taltech/a_theory2/question3contribution/Feedback2.java
+++ src/main/java/ee/taltech/a_theory2/question3contribution/Feedback2.java
@@ -10,21 +10,21 @@ public class Feedback2 {
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // 7
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
-    //
+    // I liked the funny references during the lectures, java as a language is pretty interesting. I also enjoyed the project works because I could do it with friends, but
+    // this could be not so great for people who do not have great contacts in the class.
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
-    //
+    // During part of course when we were still learning java and not doing the project, I did not really know what to do because I was not familiar with java,
+    // so I just had to wait and see the solutions to the lessons but I started understanding java and it's logic when we started the projects.
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
+    // The gitgame was a nice touch and also helped understand a bit more.
     //
 
 }
