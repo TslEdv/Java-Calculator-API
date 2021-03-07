a34507a76b5fe1858139aef0742e437ea8e4204b
Mihkel Kiil <kiil.mihkel@gmail.com>
2021-03-07 20:15:16 +0200

    addec contribution part b

--- src/main/java/ee/taltech/a_theory2/question2contribution/API1_Phones.java
+++ src/main/java/ee/taltech/a_theory2/question2contribution/API1_Phones.java
@@ -1,8 +1,11 @@
 package ee.taltech.a_theory2.question2contribution;
 
+import ee.taltech.a_theory2.question2contribution.classes.Phone;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("phones")
@@ -23,6 +26,11 @@ public class API1_Phones {
     // Management wants to define an API endpoint so frontend can display data about phones (think phone e-shop)
     // A Add necessary annotations to this class so this class can serve data
     // B Add a method to query all the phones (method content is not important - I am grading urls, annotations, names, and parameters)
+    @GetMapping("/")
+    public List<Phone> phones(){
+
+    }
+
     // C Add a method to query a single phone by it's unique identifier (method content is not important - I am grading urls, annotations, names, and parameters)
     // D Modify an existing method to query/filter phones by manufacturer while keeping existing functionality
     // E Modify an existing method to query/filter phones by release year while keeping existing functionality
