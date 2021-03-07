8ef184f21a06d38c71e6bdaaea510f9c00d7a070
Kristiina Šamanina <krsama@taltech.ee>
2021-03-07 20:37:47 +0200

    added display name to first 5 tests

--- src/test/java/ee/taltech/calculator/CalculatorServiceTest.java
+++ src/test/java/ee/taltech/calculator/CalculatorServiceTest.java
@@ -1,6 +1,7 @@
 package ee.taltech.calculator;
 
 import ee.taltech.calculator.service.CalculatorService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,21 +14,25 @@ public class CalculatorServiceTest {
     CalculatorService calculatorService = new CalculatorService();
 
     @Test
+    @DisplayName("Returns largest even number out of List")
     void maxEvenReturnsLargestEvenNumber() {
         assertEquals(6, calculatorService.maxEven(List.of(1,2,4,6)));
     }
 
     @Test
+    @DisplayName("Returns null when array is null")
     void maxEvenNullArrayReturnsNull() {
         assertNull(calculatorService.maxEven(null));
     }
 
     @Test
+    @DisplayName("Returns largest even negative number out of List")
     void maxEvenReturnsLargestEvenNegativeNumber() {
         assertEquals(-2, calculatorService.maxEven(List.of(-1,-2,-4,-6)));
     }
 
     @Test
+    @DisplayName("Returns null if list is only odd numbers")
     void maxEvenReturnsNullIfListIsOnlyOddNumbers() {
         assertNull(calculatorService.maxEven(List.of(1,-3,-5,7)));
     }
