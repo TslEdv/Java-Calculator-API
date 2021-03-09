8371843256009507f17e2a1224bf2723a531c1b8
Kristiina Šamanina <krsama@taltech.ee>
2021-03-09 15:25:41 +0200

    added stream to Even

---
+++ src/main/java/ee/taltech/calculator/util/Even.java
@@ -0,0 +1,14 @@
+package ee.taltech.calculator.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Even {
+    public List<Integer> even(List<Integer> integers) {
+      if(integers == null){
+          return null;
+      }
+      return integers.stream().filter(e-> e%2 == 0).collect(Collectors.toList());
+    }
+}
