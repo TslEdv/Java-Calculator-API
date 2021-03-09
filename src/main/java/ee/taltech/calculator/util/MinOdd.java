30a9a6af99c9601454ac0bbfe2873cb9c7ac2966
Kristiina Šamanina <krsama@taltech.ee>
2021-03-09 15:37:29 +0200

    added MinOdd

---
+++ src/main/java/ee/taltech/calculator/util/MinOdd.java
@@ -0,0 +1,12 @@
+package ee.taltech.calculator.util;
+
+import java.util.List;
+
+public class MinOdd {
+        public Integer minOdd(List<Integer> numbers) {
+            if (numbers == null) {
+                return null;
+            }
+            return numbers.stream().filter(e -> e % 2 != 0).min(Integer::compare).orElse(null);
+        }
+}
