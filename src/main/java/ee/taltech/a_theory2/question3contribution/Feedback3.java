6cd8f181afe68c3930690709d3c29a5f7e8aa2de
Kristiina Šamanina <krsama@taltech.ee>
2021-03-13 21:35:45 +0200

    added Feedback3

--- src/main/java/ee/taltech/a_theory2/question3contribution/Feedback3.java
+++ src/main/java/ee/taltech/a_theory2/question3contribution/Feedback3.java
@@ -10,21 +10,21 @@ public class Feedback3 {
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // Id give it 7, prerecorded lectures were interest to follow although id prefer more to have exercise as a whole not divided by to dos.
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
+    // I liked that we had exercises explained trough while also solved them trough.
     //
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
+    // I was struggling to understand all the logic, and there wasn't that much exercises to practise with and slides could have been a bit more detailed, for example with strema.
     //
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
+    // Git game helped a lot, I understood things after it.
     //
 
 }
