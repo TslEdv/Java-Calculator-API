464dbebb4e52e95226edf7b7107ca31f9fe296aa
Edvin Ess <edvess@taltech.ee>
2021-03-20 20:11:49 +0200

    Updated API2_Cakes

--- src/main/java/ee/taltech/a_theory2/question2contribution/API2_Cakes.java
+++ src/main/java/ee/taltech/a_theory2/question2contribution/API2_Cakes.java
@@ -1,7 +1,13 @@
 package ee.taltech.a_theory2.question2contribution;
 
+import ee.taltech.a_theory2.question2contribution.classes.Cake;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("cakes")
@@ -25,48 +31,25 @@ public class API2_Cakes {
     // C Add a method to query a single cake by it's unique identifier (method content is not important - I am grading urls, annotations, names, and parameters)
     // D Modify an existing method to query/filter cakes by weight while keeping existing functionality
     // E Modify an existing method to query/filter cakes by name while keeping existing functionality
-    // @GetMapping(value = "/")
-    // public List <Cake> cakes (@RequestParam Optional<String> name, Optional<String> weight){
-    //      if ((name == null || name == "") && (weight == null || weight == "")){
-    //          return null;
-    //      }
-    //      else if ((name == null || name == "") && (!weight.isEmpty()){
-    //          return findCake(weight);
-    //      }
-    //      else if ((weight == null || weight == "") && (!name.isEmpty()){
-    //          return findCake(name);
-    //      }
-    //      else{
-    //          return findCake(name, weight);
-    //      }
-    // }
-    // @GetMapping( value = "/cakeid")
-    // public Cake cake(@RequestParam Long id){
-    //      return cake.getCake(id);
-    // }
+    @GetMapping(value = "/")
+    public List<Cake> cakes (@RequestParam Optional<String> name, @RequestParam Optional<String> weight){
+        return null;
+    }
+    @GetMapping( value = "/cakeid")
+    public Cake cake(@RequestParam Long id){
+         return null;
+    }
     //todo theoretical assignment
     // F write pseudocode for saving a new cake (add annotations or http method names, urls, necessary parameters)
     // @GetMapping ( value = "/bakecake")
-    // public Cake bakeCake(@RequestParam Long id, String name, String sweetness, String size, String weight, String rating){
+    // public void bakeCake(@RequestBody Cake cake){
     //      Cake freshcake = new Cake();
-    //      freshcake.setId(id);
-    //      freshcake.setName(name);
-    //      freshcake.setSweetness(sweetness);
-    //      freshcake.setSize(size);
-    //      freshcake.setWeight(weight);
-    //      freshcake.setRating(rating);
-    //      return freshcake;
     // }
     // G write pseudocode for updating existing cake (add annotations or http method names, urls, necessary parameters)
     // @GetMapping ( value = "/oldcake")
-    // public void oldCake(@RequestParam Long id, Optional<String> name, Optional<String> sweetness, Optional<String> size, Optional<String> weight, Optional<String> rating){
+    // public void oldCake(@RequestParam Long id, @RequestBody Cake cake){
     //      Cake oldcake = getCake(id);
-    //      oldcake.setId(id);
-    //      oldcake.setName(name);
-    //      oldcake.setSweetness(sweetness);
-    //      oldcake.setSize(size);
-    //      oldcake.setWeight(weight);
-    //      oldcake.setRating(rating);
+    //      oldcake = cake;
     // }
     // H write pseudocode for deleting a cake (add annotations or http method names, urls, necessary parameters)
     // @GetMapping ( value = "/eatcake")
